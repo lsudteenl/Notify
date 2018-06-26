fa24217f734e09a7b1482365de2c3d286fc67b8e
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-06-26 10:44:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,24 @@
-$Token = $_GET["Token"];
-$message = $_GET["message"];
-
-line_notify($Token, $message);
-
-fucntion line_notify($Token, $message)
-{
-        $lineapi = xsWJYLzhjMRb5CF7oIwueMPOJdS8hi5c4cI7pIgY2t9; // ใส่ token key ที่ได้มา
-	$mms =  trim(xsWJYLzhjMRb5CF7oIwueMPOJdS8hi5c4cI7pIgY2t9); // ข้อความที่ต้องการส่ง
-	date_default_timezone_set("Asia/Bangkok");
-	$chOne = curl_init(); 
-	curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
-	// SSL USE 
-	curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
-	curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
-	//POST 
-	curl_setopt( $chOne, CURLOPT_POST, 1); 
-	curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$mms"); 
-	curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
-	$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$lineapi.'', );
-        curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
-	curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
-	$result = curl_exec( $chOne ); 
-	//Check error 
-	if(curl_error($chOne)) 
-	{ 
-           echo 'error:' . curl_error($chOne); 
-	} 
-	else { 
-	$result_ = json_decode($result, true); 
-	   echo "status : ".$result_['status']; echo "message : ". $result_['message'];
-        } 
-	curl_close( $chOne );   
+define(‘LINE_API’,”https://notify-api.line.me/api/notify");
+ 
+$token = “xsWJYLzhjMRb5CF7oIwueMPOJdS8hi5c4cI7pIgY2t9”; //ใส่Token ที่copy เอาไว้
+$str = “Hello”; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+$res = notify_message($str,$token);
+print_r($res);
+function notify_message($message,$token){
+ $queryData = array(‘message’ => $message);
+ $queryData = http_build_query($queryData,’’,’&’);
+ $headerOptions = array( 
+         ‘http’=>array(
+            ‘method’=>’POST’,
+            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
+                      .”Authorization: Bearer “.$token.”\r\n”
+                      .”Content-Length: “.strlen($queryData).”\r\n”,
+            ‘content’ => $queryData
+         ),
+ );
+ $context = stream_context_create($headerOptions);
+ $result = file_get_contents(LINE_API,FALSE,$context);
+ $res = json_decode($result);
+ return $res;
 }
